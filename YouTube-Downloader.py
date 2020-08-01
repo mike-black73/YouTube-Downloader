afb989a28ec201b7c6720071ed17f06f94ad3520
mike-black73 <blackxkingxdragon@gmail.com>
2020-08-01 15:36:36 +0200

    YouTube-Downloader

---
+++ YouTube-Downloader.py
@@ -0,0 +1,13 @@
+import re
+from pytube import Playlist
+from pytube import YouTube
+
+print('put the video/playlist url here:')
+link = input()
+
+playlist = Playlist(link)
+playlist._video_regex = re.compile(r"\"url\":\"(/watch\?v=[\w-]*)")
+print('Number of videos in playlist: %s' % len(playlist.video_urls))
+for url in playlist.video_urls:
+   print(url)
+   YouTube(url).streams.first().download()
